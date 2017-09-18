f22441fc5b672415551a416dd41511df0578ab9a
Julian Maurice <julian.maurice@biblibre.com>
2017-09-19 00:55:24 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,17 @@
+# Omeka addons index
+
+This repository contains an exhaustive list of plugins and themes published on
+Omeka.org along with the information contained in `plugin.ini` and `theme.ini`.
+
+It also contains the script that generates this list and it is launched
+periodically by [Travis CI](https://travis-ci.org), so this is list is kept
+up-to-date automatically.
+
+[![Build Status](https://travis-ci.org/jajm/omeka-addons-index.svg?branch=master)](https://travis-ci.org/jajm/omeka-addons-index)
+
+* [List of plugins](data/plugins.json)
+* [List of themes](data/themes.json)
+
+## License
+
+GPL 3.0+
---
+++ src/Addons.php
@@ -0,0 +1,110 @@
+<?php
+
+/*
+ * Copyright (C) 2017  Julian Maurice
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+namespace OmekaAddonsIndex;
+
+use DateTime;
+use ZipArchive;
+use Goutte\Client;
+
+abstract class Addons
+{
+    protected $url;
+    protected $iniFilename;
+
+    public function update($filename)
+    {
+        $oldAddons = [];
+        $addonsByUrl = [];
+
+        if (file_exists($filename)) {
+            $oldAddons = json_decode(file_get_contents($filename), true);
+
+            foreach ($oldAddons as $addonDirName => $addon) {
+                foreach ($addon['versions'] as &$version) {
+                    $addonsByUrl[$version['url']] = [
+                        'name' => $addonDirName,
+                        'version' => $version,
+                    ];
+                }
+            }
+        }
+
+        $client = new Client();
+        $root = $client->request('GET', $this->url);
+        $downloadLinks = $root
+            ->filter('a.omeka-addons-button');
+
+        $addons = [];
+        for ($i = 0; $i < count($downloadLinks); ++$i) {
+            $downloadLink = $downloadLinks->eq($i);
+            $addonLink = $this->getAddonLinkFromDownloadLink($downloadLink);
+
+            $page = $client->request('GET', $addonLink->link()->getUri());
+            $trs = $page->filter('table.omeka-addons-versions tr');
+            for ($j = 1; $j < count($trs); ++$j){
+                $tr = $trs->eq($j);
+                $url = $tr->filter('a')->link()->getUri();
+
+                if (array_key_exists($url, $addonsByUrl)) {
+                    fwrite(STDERR, "Skipping $url\n");
+
+                    $oldPlugin = $addonsByUrl[$url];
+                    $addonDirName = $oldPlugin['name'];
+                    $addonVersion = &$oldPlugin['version'];
+                } else {
+                    $zip = tempnam(sys_get_temp_dir(), 'omeka-addon.');
+
+                    fwrite(STDERR, "Downloading $url...\n");
+                    file_put_contents($zip, fopen($url, 'r'));
+
+                    // Get addon's directory name and contents of addon.ini
+                    $zipArchive = new ZipArchive();
+                    $zipArchive->open($zip);
+                    $name = $zipArchive->getNameIndex(0);
+                    $dirname = dirname($name);
+                    $addonDirName = ($dirname === '.') ? $name : $dirname;
+                    $addonDirName = rtrim($addonDirName, '/');
+                    $iniPath = $addonDirName . '/' . $this->iniFilename;
+                    $ini = $zipArchive->getFromName($iniPath);
+                    $zipArchive->close();
+                    unlink($zip);
+
+                    // Release date
+                    $date = $tr->filter('td')->eq(3)->text();
+                    $dt = new DateTime($date);
+
+                    $addonVersion = [
+                        'url' => $url,
+                        'date' => $dt->format('Y-m-d'),
+                        'info' => parse_ini_string($ini),
+                    ];
+                }
+
+                $addons[$addonDirName]['versions'][] = $addonVersion;
+            }
+        }
+
+        ksort($addons);
+
+        file_put_contents($filename, json_encode($addons, JSON_PRETTY_PRINT));
+    }
+
+    abstract protected function getAddonLinkFromDownloadLink($downloadLink);
+}
