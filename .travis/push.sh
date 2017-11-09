cfb372d5bdbbb5352bf6430afe6da1b92b6534a7
Julian Maurice <julian.maurice@biblibre.com>
2017-11-09 15:30:36 +0100

    Travis: Change destination repository

--- .travis/push.sh
+++ .travis/push.sh
@@ -6,4 +6,4 @@ git config user.name "Travis CI"
 git add data
 git commit -m "Travis build: $TRAVIS_BUILD_NUMBER"
 
-git push --quiet https://${GH_TOKEN}@github.com/jajm/omeka-addons-index.git HEAD:$TRAVIS_BRANCH
+git push --quiet https://${GH_TOKEN}@github.com/biblibre/omeka-addons-index.git HEAD:$TRAVIS_BRANCH
